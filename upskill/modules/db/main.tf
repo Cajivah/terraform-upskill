a5c98f79d05401e9fa9aebc09dd58ed77dc15a84
Jakub Małyjasiak <jmalyjasiak@pgs-soft.com>
2020-02-02 08:57:19 +0100

    Take credentials from parameter store.

--- upskill/modules/db/main.tf
+++ upskill/modules/db/main.tf
@@ -5,15 +5,15 @@ resource "aws_db_instance" "db" {
   engine_version    = var.engine_version
   allocated_storage = var.allocated_storage
   instance_class    = var.instance_type
+  skip_final_snapshot    = true
 
   name     = var.db_name
-  username = var.username
-  password = var.db_password
+  username = data.aws_ssm_parameter.db_username
+  password = data.aws_ssm_parameter.db_password
 
-  vpc_security_group_ids    = var.security_group_ids
-  db_subnet_group_name      = aws_db_subnet_group.db_subnet.name
-  final_snapshot_identifier = "${var.env}-${var.db_name}"
-  tags                      = var.tags
+  vpc_security_group_ids = var.security_group_ids
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
+  tags                   = var.tags
 }
 
 resource "aws_db_subnet_group" "db_subnet" {
